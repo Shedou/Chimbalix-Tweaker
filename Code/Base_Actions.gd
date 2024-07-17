a75876f874926196db6ede4c725e07aaabfe1bfd
Chimbal <chimbal.chimbal@yandex.ru>
2024-07-17 21:39:41 +0400

    locales update

--- Code/Base_Actions.gd
+++ Code/Base_Actions.gd
@@ -7,6 +7,7 @@ var work_dir :String;
 
 func on_ready():
 	work_dir = $"..".work_dir;
+	localize();
 
 func _on_clear_thumbcache_pressed():
 	code = OS.execute("terminal", ["-e", work_dir+"/Scripts/clear-thumb-cache.sh"], false, output, false, false);
@@ -25,3 +26,17 @@ func _on_drop_caches_all_pressed():
 
 func _on_clean_xsession_pressed():
 	code = OS.execute("terminal", ["-e", work_dir+"/Scripts/clean-x-errors-log.sh"], false, output, false, false);
+
+func localize():
+	$base_actions_hint.hint_tooltip = $"%str".Base_Actions.hint_base_actions_hint;
+	$base_actions_hint.text = $"%str".Base_Actions.text_base_actions_hint;
+	
+	$clear_thumbcache.hint_tooltip = $"%str".Base_Actions.hint_clear_thumbcache;
+	$clear_thumbcache.text = $"%str".Base_Actions.text_clear_thumbcache;
+	
+	$restart_xfcepanel.hint_tooltip = $"%str".Base_Actions.hint_restart_xfcepanel;
+	$restart_xfcepanel.text = $"%str".Base_Actions.text_restart_xfcepanel;
+	
+	$restart_thunar.hint_tooltip = $"%str".Base_Actions.hint_restart_thunar;
+	$restart_thunar.text = $"%str".Base_Actions.text_restart_thunar;
+
--- Code/string.gd
+++ Code/string.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Стринги
 
-var base :Dictionary = {
+var base : Dictionary = {
 	"capt_warning":"WARNING!",
 	"capt_attention":"ATTENTION!",
 	"wrong":"Something went wrong...",
@@ -14,6 +14,18 @@ var base :Dictionary = {
 	"work_dir_arg_not_found_tip":"Please run the application correctly: chimbalix-tweaker \"/path/to/executable_dir\"",
 	};
 
-var Main_Menu_Items :Dictionary = {
-	"hint_about_current_work_dir":"The application's working directory."
+var Main_Menu_Items : Dictionary = {
+	"hint_about_current_work_dir":"The application's working directory"
 	};
+
+var Base_Actions : Dictionary = {
+	"hint_base_actions_hint":"Tooltip",
+	"hint_clear_thumbcache":"Clear the image preview cache located in:\n\"/home/USER_FOLDER/.cache/thumbnails/\".",
+	"hint_restart_xfcepanel":"Command \"xfce4-panel -r\".\nUseful when items get stuck in the Start menu.",
+	"hint_restart_thunar":"Closes all \"thunar\" processes and starts again with the command:\nnohup thunar --daemon &",
+	"text_base_actions_hint":"* - Requires root rights. Hover your mouse over to get a hint.",
+	"text_clear_thumbcache":"Clear Thumbnail Cache",
+	"text_restart_xfcepanel":"Restart Xfce4 Panel",
+	"text_restart_thunar":"Restart Thunar (Explorer)",
+	};
+
